42265589bd49486b88fad0e1e23e6e7701b11bbe
vishalrkothekar@gmail.com <vishalrkothekar@gmail.com>
2020-10-16 16:44:58 +0530

    change share screen button

--- bigbluebutton-html5/imports/ui/components/actions-bar/component.jsx
+++ bigbluebutton-html5/imports/ui/components/actions-bar/component.jsx
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react';
 import cx from 'classnames';
 import { styles } from './styles.scss';
 import DesktopShare from './desktop-share/component';
-import ActionsDropdown from './actions-dropdown/component';
+// import ActionsDropdown from './actions-dropdown/component';
 import QuickPollDropdown from './quick-poll-dropdown/component';
 import AudioControlsContainer from '../audio/audio-controls/container';
 import JoinVideoOptionsContainer from '../video-provider/video-button/container';
@@ -16,24 +16,24 @@ class ActionsBar extends PureComponent {
       handleShareScreen,
       handleUnshareScreen,
       isVideoBroadcasting,
-      amIModerator,
+      // amIModerator,
       screenSharingCheck,
       enableVideo,
       isLayoutSwapped,
       toggleSwapLayout,
-      handleTakePresenter,
+      // handleTakePresenter,
       intl,
       currentSlidHasContent,
       parseCurrentSlideContent,
-      isSharingVideo,
+      // isSharingVideo,
       screenShareEndAlert,
-      stopExternalVideoShare,
+      // stopExternalVideoShare,
       screenshareDataSavingSetting,
       isCaptionsAvailable,
       isMeteorConnected,
       isPollingEnabled,
       isThereCurrentPresentation,
-      allowExternalVideo,
+      // allowExternalVideo,
     } = this.props;
 
     const actionBarClasses = {};
@@ -45,7 +45,7 @@ class ActionsBar extends PureComponent {
     return (
       <div className={styles.actionsbar}>
         <div className={styles.left}>
-          <ActionsDropdown {...{
+          {/* <ActionsDropdown {...{
             amIPresenter,
             amIModerator,
             isPollingEnabled,
@@ -55,6 +55,17 @@ class ActionsBar extends PureComponent {
             isSharingVideo,
             stopExternalVideoShare,
             isMeteorConnected,
+          }}
+          /> */}
+          <DesktopShare {...{
+            handleShareScreen,
+            handleUnshareScreen,
+            isVideoBroadcasting,
+            amIPresenter,
+            screenSharingCheck,
+            screenShareEndAlert,
+            isMeteorConnected,
+            screenshareDataSavingSetting,
           }}
           />
           {isPollingEnabled
@@ -83,17 +94,7 @@ class ActionsBar extends PureComponent {
               <JoinVideoOptionsContainer />
             )
             : null}
-          <DesktopShare {...{
-            handleShareScreen,
-            handleUnshareScreen,
-            isVideoBroadcasting,
-            amIPresenter,
-            screenSharingCheck,
-            screenShareEndAlert,
-            isMeteorConnected,
-            screenshareDataSavingSetting,
-          }}
-          />
+          
         </div>
         <div className={styles.right}>
           {isLayoutSwapped
